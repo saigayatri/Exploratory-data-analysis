f66f79ad7fc1e9052dc9d0f58580800b1ff62681
saigayatri <saigayatri69@gmail.com>
2016-12-26 17:13:56 +0530

    Create edaplot3.r

---
+++ edaplot3.r
@@ -0,0 +1,17 @@
+dataFile <- "D:/3-2/ household_power_consumption.txt"
+data <- read.table("D:/3-2/household_power_consumption.txt", header=TRUE, sep=";", stringsAsFactors=FALSE, dec=".")
+subsetData <- data[data$Date %in% c("1/2/2007","2/2/2007") ,]
+
+#str(subSetData)
+datetime <- strptime(paste(subsetData$Date, subsetData$Time, sep=" "), "%d/%m/%Y %H:%M:%S") 
+globalActivePower <- as.numeric(subsetData$Global_active_power)
+submetering1 <- as.numeric(subsetData$Sub_metering_1)
+submetering2 <- as.numeric(subsetData$Sub_metering_2)
+submetering3 <- as.numeric(subsetData$Sub_metering_3)
+
+png("plot3.png", width=480, height=480)
+plot(datetime, subMetering1, type="l", ylab="Energy Submetering", xlab="")
+lines(datetime, subMetering2, type="l", col="red")
+lines(datetime, subMetering3, type="l", col="blue")
+legend("topright", c("Sub_metering_1", "Sub_metering_2", "Sub_metering_3"), lty=1, lwd=2.5, col=c("black", "red", "blue"))
+dev.off()
